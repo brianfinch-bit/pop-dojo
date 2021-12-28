6344f4c75ac0d4fc6dfc19c45bda78591972c0f5
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-12-28 21:40:27 +0700

    Update and rename tes.js to tessss.js

--- tessss.js
+++ tessss.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
   atOptions = {
     'key' : '0c36e62040e13f23ed831c26d97931ec',
     'format' : 'iframe',
@@ -7,4 +6,3 @@
     'params' : {}
   };
   document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
-</script>
